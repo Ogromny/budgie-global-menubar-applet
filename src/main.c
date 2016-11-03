72e646a6518117918609a9e47a1d75b440abe47c
Ikey Doherty <ikey@solus-project.com>
2016-11-03 21:51:46 +0000

    Stub the query_window_menu methods
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

--- src/main.c
+++ src/main.c
@@ -9,7 +9,10 @@
  * (at your option) any later version.
  */
 
+#include <stdio.h>
 #include <stdlib.h>
+
+#include "win-query.h"
 
 /**
  * Current unity-gtk-greeter export path
@@ -23,7 +26,34 @@
 
 int main(int argc, char **argv)
 {
-        return EXIT_FAILURE;
+        int ret = EXIT_SUCCESS;
+
+        if (argc < 2) {
+                fprintf(stderr, "Provide a window id\n");
+                return EXIT_FAILURE;
+        }
+        const char *xwind = argv[1];
+        gulong id = g_ascii_strtoull(xwind, NULL, 10);
+
+        gchar *tmp = query_window_menu_object_path(id);
+        if (tmp) {
+                fprintf(stderr, "Got ID: %s\n", tmp);
+                goto finish;
+        }
+        tmp = query_window_menu_object_path_legacy(id);
+        if (tmp) {
+                fprintf(stderr, "Got legacy ID: %s\n", tmp);
+                goto finish;
+        }
+
+        ret = EXIT_FAILURE;
+        fprintf(stderr, "No ID found for: %lu\n", id);
+finish:
+
+        if (tmp) {
+                free(tmp);
+        }
+        return ret;
 }
 
 /*
---
+++ src/win-query.c
@@ -0,0 +1,41 @@
+/*
+ * This file is part of budgie-global-menubar-applet
+ *
+ * Copyright © 2016 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include "win-query.h"
+
+/**
+ * Return the dbus menu path for a given window
+ */
+gchar *query_window_menu_object_path(gulong xid)
+{
+        return NULL;
+}
+
+/**
+ * Return the legacy dbus menu path for a given window.
+ */
+gchar *query_window_menu_object_path_legacy(gulong xid)
+{
+        return NULL;
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
